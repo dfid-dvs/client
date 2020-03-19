9111106171f192d95f5523fb53213065dfb7abf0
tnagorra <weathermist@gmail.com>
2020-03-19 15:44:59 +0545

    Remove scss-lint
    - Add stylelint in bulid pipeline
    - Add buttons

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -17,10 +17,101 @@ const Dashboard = (props: Props) => {
             className,
         )}
         >
-            Dashboard
-            <Button>
-                button
-            </Button>
+            <div>
+                Dashboard
+            </div>
+            <div className={styles.buttons}>
+                <Button
+                    className={styles.button}
+                    onClick={() => {
+                        console.warn('clicked');
+                    }}
+                >
+                    default
+                </Button>
+                <Button
+                    variant="primary"
+                    className={styles.button}
+                    onClick={() => {
+                        console.warn('clicked');
+                    }}
+                >
+                    primary
+                </Button>
+                <Button
+                    variant="accent"
+                    className={styles.button}
+                    onClick={() => {
+                        console.warn('clicked');
+                    }}
+                >
+                    accent
+                </Button>
+                <Button
+                    variant="warning"
+                    className={styles.button}
+                    onClick={() => {
+                        console.warn('clicked');
+                    }}
+                >
+                    warning
+                </Button>
+                <Button
+                    variant="danger"
+                    className={styles.button}
+                    onClick={() => {
+                        console.warn('clicked');
+                    }}
+                >
+                    danger
+                </Button>
+                <Button
+                    variant="success"
+                    className={styles.button}
+                    onClick={() => {
+                        console.warn('clicked');
+                    }}
+                >
+                    success
+                </Button>
+            </div>
+            <div className={styles.buttons}>
+                <Button
+                    className={styles.button}
+                >
+                    default
+                </Button>
+                <Button
+                    variant="primary"
+                    className={styles.button}
+                >
+                    primary
+                </Button>
+                <Button
+                    variant="accent"
+                    className={styles.button}
+                >
+                    accent
+                </Button>
+                <Button
+                    variant="warning"
+                    className={styles.button}
+                >
+                    warning
+                </Button>
+                <Button
+                    variant="danger"
+                    className={styles.button}
+                >
+                    danger
+                </Button>
+                <Button
+                    variant="success"
+                    className={styles.button}
+                >
+                    success
+                </Button>
+            </div>
         </div>
     );
 };
