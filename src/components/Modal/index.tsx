c0485e90ae291fb5344f6b8dd200ddb7ce6e9f12
Aditya Khatri <adityakhatri47@gmail.com>
2020-06-09 12:40:41 +0545

    Add modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,82 @@
+import React, { useCallback } from 'react';
+import { _cs } from '@togglecorp/fujs';
+import { IoMdClose } from 'react-icons/io';
+
+import Button from '#components/Button';
+import Backdrop from '#components/Backdrop';
+import Portal from '#components/Portal';
+
+import styles from './styles.css';
+
+interface Props {
+    children?: React.ReactNode;
+    header?: React.ReactNode;
+    footer?: React.ReactNode;
+    className?: string;
+    bodyClassName?: string;
+    headerClassName?: string;
+    footerClassName?: string;
+    onClose?: () => void;
+}
+
+function Modal(props: Props) {
+    const {
+        header,
+        children,
+        footer,
+
+        className,
+        headerClassName,
+        bodyClassName,
+        footerClassName,
+
+        onClose,
+    } = props;
+
+    const handleCloseButtonClick = useCallback(
+        () => {
+            if (onClose) {
+                onClose();
+            }
+        },
+        [onClose],
+    );
+
+    return (
+        <Portal>
+            <Backdrop dark>
+                <div
+                    className={_cs(
+                        className,
+                        styles.modal,
+                    )}
+                >
+                    <div className={_cs(styles.modalHeader, headerClassName)}>
+                        <div className={styles.titleContainer}>
+                            { header }
+                        </div>
+                        <Button
+                            className={styles.closeButton}
+                            onClick={handleCloseButtonClick}
+                            transparent
+                            variant="danger"
+                            title="Close"
+                        >
+                            <IoMdClose />
+                        </Button>
+                    </div>
+                    <div className={_cs(styles.modalBody, bodyClassName)}>
+                        { children }
+                    </div>
+                    {footer && (
+                        <div className={_cs(styles.modalFooter, footerClassName)}>
+                            {footer}
+                        </div>
+                    )}
+                </div>
+            </Backdrop>
+        </Portal>
+    );
+}
+
+export default Modal;
