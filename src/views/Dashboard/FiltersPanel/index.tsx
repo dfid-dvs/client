86ddddb35f9a393bab76107136371f426922745f
ShrawanBk <bkshrawanbk@gmail.com>
2021-03-26 10:16:34 +0545

    Add date range selector in dashboard

--- src/views/Dashboard/FiltersPanel/index.tsx
+++ src/views/Dashboard/FiltersPanel/index.tsx
@@ -4,17 +4,23 @@ import ProgramSelector from '#components/ProgramSelector';
 interface FiltersPanelProps {
     className?: string;
     isMinimized?: boolean;
+    startDate?: string;
+    endDate?: string;
 }
 export default function FiltersPanel(props: FiltersPanelProps) {
     const {
         className,
         isMinimized,
+        startDate,
+        endDate,
     } = props;
 
     return (
         <ProgramSelector
             className={className}
             isMinimized={isMinimized}
+            startDate={startDate}
+            endDate={endDate}
         />
     );
 }
