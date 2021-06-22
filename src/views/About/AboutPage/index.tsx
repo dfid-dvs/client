581d35e8c713985b0c6017a6da980c1d24a361a1
ShrawanBk <bkshrawanbk@gmail.com>
2021-06-22 09:09:32 +0545

    Update about page

--- src/views/About/AboutPage/index.tsx
+++ src/views/About/AboutPage/index.tsx
@@ -43,6 +43,18 @@ const articleTitle = 'Following successful elections in late 2017';
 // eslint-disable-next-line max-len
 const articleDescription = 'Nepal has the potential for higher, inclusive economic growth through the development of hydro-electric power and through sectors like agro-processing, light manufacturing and tourism, which present significant opportunities for UK business, and trade with India and China. This potential is hampered by complex investment rules and processes, costly and unreliable energy supply, poor transport infrastructure, political instability, weak institutions, poor governance and gender disparity. Nepal is highly vulnerable to natural disasters and climate change which can push populations back into poverty, destroy infrastructure and undermine growth. The 2015 earthquakes caused extensive damage and Nepal remains at high risk of a catastrophic earthquake.';
 
+interface AboutUs {
+    count: number;
+    next?: number;
+    previous?: number;
+    results: {
+        id: number;
+        title: string;
+        subTitle: string;
+        body: string;
+    }[];
+}
+
 interface Summary {
     allocatedBudget: number;
     program: number;
@@ -61,9 +73,28 @@ interface Manual {
     }[];
 }
 
-export default function AboutPage() {
-    const summaryUrl = `${apiEndPoint}/core/summary/`;
+interface ContactUs {
+    count: number;
+    next?: number;
+    previous?: number;
+    results: {
+        id: number;
+        name: string;
+        address: string;
+        email: string;
+        telephone: string;
+        fax: string;
+    }[];
+}
 
+export default function AboutPage() {
+    const aboutUsUrl = `${apiEndPoint}/about_us/`;
+    const [
+        aboutUsPending,
+        aboutUsResponse,
+    ] = useRequest<AboutUs>(aboutUsUrl, 'about-us');
+
+    const summaryUrl = `${apiEndPoint}/core/summary/`;
     const [
         summaryPending,
         summary,
@@ -75,20 +106,44 @@ export default function AboutPage() {
         manual,
     ] = useRequest<Manual>(manualUrl, 'manual-data');
 
-    const loading = summaryPending || manualPending;
+    const contactUsUrl = `${apiEndPoint}/about_us/contact_us`;
+    const [
+        contactUsPending,
+        contactUsResponse,
+    ] = useRequest<ContactUs>(contactUsUrl, '');
+
+    const loading = summaryPending || manualPending || aboutUsPending || contactUsPending;
 
     const manualLink = manual?.results[0].file;
+    const aboutUs = aboutUsResponse?.results[0];
+    const contactUs = contactUsResponse?.results[0];
+
     return (
         <AboutPageContainer>
             <div className={styles.container}>
                 <div className={styles.firstSection}>
-                    <div className={styles.title}>
-                        {firstSectionTitle}
-                    </div>
-                    <div className={styles.subTitle}>
-                        {firstSectionSubTitle}
-                    </div>
+                    {aboutUs?.title && (
+                        <div
+                            className={styles.title}
+                            dangerouslySetInnerHTML={{ __html: aboutUs.title }}
+                        />
+                    )}
+                    {aboutUs?.subTitle && (
+                        <div
+                            className={styles.subTitle}
+                            dangerouslySetInnerHTML={{ __html: aboutUs.subTitle }}
+                        />
+                    )}
                 </div>
+                <a
+                    className={styles.manualLink}
+                    href={manualLink}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                >
+                    <IoMdDocument className={styles.manualIcon} />
+                    View Manual
+                </a>
                 <div className={styles.stats}>
                     {loading && (
                         <Backdrop>
@@ -116,72 +171,47 @@ export default function AboutPage() {
                         label="Sectors"
                     />
                 </div>
-                <div className={styles.details}>
-                    <div className={styles.left}>
-                        <div className={styles.title}>
-                            {articleTitle}
-                        </div>
-                        <div className={styles.description}>
-                            {articleDescription}
-                        </div>
-                        <div className={styles.list}>
-                            {listedData.map(data => (
-                                <div
-                                    key={data}
-                                    className={styles.listItem}
-                                >
-                                    <IoMdCheckmarkCircleOutline className={styles.checkBox} />
-                                    <div className={styles.description}>
-                                        {data}
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        <a
-                            className={styles.manualLink}
-                            href={manualLink}
-                            rel="noreferrer noopener"
-                            target="_blank"
-                        >
-                            <IoMdDocument className={styles.manualIcon} />
-                            View Manual
-                        </a>
-                    </div>
-                    <div className={styles.right}>
-                        <img
-                            src={aboutUsImage}
-                            alt="British Embassy Kathmandu"
-                            className={styles.image}
-                        />
-                    </div>
-                </div>
+                {aboutUs?.body && (
+                    <div
+                        className={styles.details}
+                        dangerouslySetInnerHTML={{ __html: aboutUs.body }}
+                    />
+                )}
                 <div className={styles.contact}>
                     <div className={styles.left}>
                         Contact us
                     </div>
-                    <div className={styles.right}>
-                        <div className={styles.contactDetails}>
-                            <div className={styles.name}>
-                                {name}
-                            </div>
-                            <div className={styles.address}>
-                                {address}
-                            </div>
-                            {contactDetails.map(data => (
-                                <div
-                                    key={data.title}
-                                    className={styles.contactItem}
-                                >
-                                    <div className={styles.contactTitle}>
-                                        {data.title}
-                                    </div>
-                                    <div>
-                                        {data.value}
-                                    </div>
+                    {contactUs && (
+                        <div className={styles.right}>
+                            <div className={styles.contactDetails}>
+                                <div className={styles.name}>
+                                    {contactUs.name}
                                 </div>
-                            ))}
+                                <div
+                                    className={styles.address}
+                                    dangerouslySetInnerHTML={{ __html: contactUs.address }}
+                                />
+                                <div className={styles.contactItem}>
+                                    <div className={styles.contactTitle}>
+                                        Email
+                                    </div>
+                                    {contactUs.email}
+                                </div>
+                                <div className={styles.contactItem}>
+                                    <div className={styles.contactTitle}>
+                                        Tel
+                                    </div>
+                                    {contactUs.telephone}
+                                </div>
+                                <div className={styles.contactItem}>
+                                    <div className={styles.contactTitle}>
+                                        Fax
+                                    </div>
+                                    {contactUs.fax}
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </AboutPageContainer>
