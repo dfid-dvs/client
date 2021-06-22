47d1ddce529fb9c23fca7ada99b1f688122cfe81
ShrawanBk <bkshrawanbk@gmail.com>
2021-06-22 09:12:25 +0545

    Remove unused variable and image

--- src/views/About/AboutPage/index.tsx
+++ src/views/About/AboutPage/index.tsx
@@ -1,47 +1,15 @@
 import React from 'react';
-import { IoMdCheckmarkCircleOutline, IoMdDocument } from 'react-icons/io';
+import { IoMdDocument } from 'react-icons/io';
 
 import useRequest from '#hooks/useRequest';
 import { apiEndPoint } from '#utils/constants';
 
 import LoadingAnimation from '#components/LoadingAnimation';
 import Backdrop from '#components/Backdrop';
-import aboutUsImage from '#resources/about-us.jpg';
 
 import StatItem from './StatItem';
 import styles from './styles.css';
 import AboutPageContainer from '../AboutPageContainer';
-
-const listedData = [
-    'Development Tracker to explore international development projects funded by the UK government by country and sector',
-    'International Development Funding finder tool for funding opportunities by country, sector and organisation type',
-    'Research for Development Outputs for outputs from DFID funded research projects and programmes',
-];
-
-const contactDetails = [
-    {
-        title: 'Email',
-        value: 'nepal-enquiries@dfid.gov.uk',
-    },
-    {
-        title: 'Tel',
-        value: '+977 1 4237100',
-    },
-    {
-        title: 'Fax',
-        value: '+977 1 4411789',
-    },
-];
-const name = 'British Embassy Kathmandu';
-const address = 'British Embassy\nPO Box 106\nKathmandu, Nepal';
-// eslint-disable-next-line max-len
-const firstSectionTitle = 'Find out how the UK will respond to opportunities and challenges, what is being achieved for the UK and who we are working with.';
-// eslint-disable-next-line max-len
-const firstSectionSubTitle = 'The Department for International Development (DFID) closed on 2 September 2020 and merged with the Foreign and Commonwealth Office (FCO) to create the Foreign, Commonwealth and Development Office.';
-
-const articleTitle = 'Following successful elections in late 2017';
-// eslint-disable-next-line max-len
-const articleDescription = 'Nepal has the potential for higher, inclusive economic growth through the development of hydro-electric power and through sectors like agro-processing, light manufacturing and tourism, which present significant opportunities for UK business, and trade with India and China. This potential is hampered by complex investment rules and processes, costly and unreliable energy supply, poor transport infrastructure, political instability, weak institutions, poor governance and gender disparity. Nepal is highly vulnerable to natural disasters and climate change which can push populations back into poverty, destroy infrastructure and undermine growth. The 2015 earthquakes caused extensive damage and Nepal remains at high risk of a catastrophic earthquake.';
 
 interface AboutUs {
     count: number;
